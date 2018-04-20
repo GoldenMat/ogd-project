be73ed5b5ee97d9bbf7fdaddda6dfde068f1a581
Unknown <maura.sacca@gmail.com>
2018-04-20 12:33:46 +0200

    Primo commit ProgettoYolt (Scene con Skill ad area)

---
+++ Yolt/Assets/Script/Cono.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cono : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll) {
+
+        if (coll.gameObject.tag == "Enemy") {
+            //coll.gameObject.GetComponent<Health>().TakeDamege(param)
+            coll.transform.gameObject.SendMessage("ConeDamage");
+        }
+
+    }
+}
---
+++ Yolt/Assets/Script/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int _health;
+
+	// Use this for initialization
+	void Start () {
+		_health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AreaDamage(){
+		_health -= 10;
+
+		//Debug.Log ("Health: " + _health);
+	}
+
+	void AreaHeal(){
+		_health += 20;
+	}
+
+    public void TakeDotDamage() { }
+
+    public void TakeDamage(int dam) {
+        _health -= dam;
+    }
+
+
+}
