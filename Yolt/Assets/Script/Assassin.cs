67ee17d03cfae45b0362b9409c40a0eb43c7ec7a
Unknown <maura.sacca@gmail.com>
2018-05-28 15:22:50 +0200

    stable revert

---
+++ Yolt/Assets/Script/Assassin.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assassin : MonoBehaviour {
+
+	public Camera cam;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+
+			//Debug.Log ("Click Mouse");
+		
+			/*
+			Vector3 pos = Input.mousePosition;
+			pos.z = cam.nearClipPlane;
+
+			Debug.Log ("Pos" + pos);
+			pos = Camera.main.ScreenToWorldPoint (pos);
+			pos.y = 0;
+			Debug.Log ("PosDopoScale" + pos);
+			*/
+
+			Ray pos = cam.ScreenPointToRay (Input.mousePosition);
+			Debug.DrawRay (pos.origin, pos.direction *30, Color.yellow, 100);
+            RaycastHit hit;
+
+			if(Physics.Raycast(pos, out hit)) {
+                Vector3 bas = hit.collider.bounds.center;
+                bas.y = 0;
+
+                if (hit.collider.gameObject.tag == "Floor")
+                {
+                    bas = hit.point;
+                }
+
+                DebugExtension.DebugWireSphere (bas, 2.5f, 100, true);
+
+				Collider[] Arround = Physics.OverlapSphere(bas, 2.5f);
+				foreach (Collider intoExp in Arround) {
+					if (intoExp.transform.tag == "Enemy") {
+						intoExp.GetComponent<BoxCollider> ().transform.gameObject.SendMessage ("AreaDamage");
+						//Debug.Log ("Message Sent");
+					}
+				}
+
+            }
+
+
+                
+
+			}
+
+
+		//cono
+		if (Input.GetMouseButtonDown (1)) {
+
+            Debug.Log("Mouse");
+
+            transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+        }
+
+        if (Input.GetMouseButtonUp(1)) {
+
+            transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+
+            transform.GetChild(0).GetComponent<MeshCollider>().enabled = true;
+
+
+
+
+
+
+        }
+		
+	}
+
+
+}
